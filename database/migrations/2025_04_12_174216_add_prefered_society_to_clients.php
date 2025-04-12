ae1af6445f09031b4c9f851ed5e32a0abf967de9
Johan J <johan.janin@gmail.com>
2025-04-12 19:46:49 +0200

    Added prefered society column

---
+++ database/migrations/2025_04_12_174216_add_prefered_society_to_clients.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\Society;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->foreignId('prefered_society')->nullable()->constrained('societies')->nullOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->dropColumn('prefered_society');
+        });
+    }
+};
