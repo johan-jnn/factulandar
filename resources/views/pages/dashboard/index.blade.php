a88d3597b8440eee80d69ffff4a083e8652c5d7f
Johan J <johan.janin@gmail.com>
2025-04-13 11:17:30 +0200

    css bug fixes

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -8,3 +8,43 @@
 @extends('layouts.dashboard', [
     'page' => 'Clients',
 ])
+
+@section('body')
+    <main>
+        <h1>Bienvenue, {{ $user->name }}</h1>
+
+        <section id="societies">
+            <h2>Vos sociétés</h2>
+            <p>Sur <strong>Factulandar</strong>, vous pouvez posséder autant de sociétés que vous le souhaitez.</p>
+            {{-- <p>Ces sociétés permettrons de </p> --}}
+            <p>Vos sociétés sont accécibles sur <a href="{{ route('account') }}">la page de votre compte</a>.</p>
+        </section>
+        <section id="clients">
+            <h2>Vos clients</h2>
+            <p>Vous pouvez retrouver la liste de vos clients sur la partie gauche de cette interface.</p>
+            <p>En cliquant sur le bouton <button type="button" disabled>➕</button>, vous pouvez ajouter un nouveau client.
+            </p>
+            <p>Vous pouvez également rechercher des clients en appuyant sur le bouton <button type="button"
+                    disabled>🔍️</button>.</p>
+            <hr>
+            <p>Après avoir sélectionner un client, vous trouverez les informations sur celui-ci comme le nombre de facture
+                faites, le calendrier associé, etc...</p>
+            <p>Vous pouvez venir modifier le client dans le menu <i>Modifier le client</i> à gauche de la page client.</p>
+        </section>
+        <section id="invoices">
+            <h2>Générer une facture</h2>
+            <p>Dans la rubrique <i>Factures</i> de votre client, vous retrouverez la liste des factures associés à ce
+                client.</p>
+            <p>Vous pouvez cliquer sur un de ces factures pour retrouver la retrouver. Notez qu'une fois la facture validée,
+                vous pourrez uniquement la supprimer ou la dupliquer, mais en aucun elle ne pourra être modifiée.</p>
+            <p>Pour créer une nouvelle facture, vous pouvez soit dupliquer une facture existante, soit appuyer sur le bouton
+                <button type="button">Créer une facture</button> qui vous emmenera sur la page de création de factures.
+            </p>
+        </section>
+        <section id="info">
+            <h2>Informations</h2>
+            <p>Ce projet a été conçu pour un devoir d'étudiant chez My Digital School (Lyon).</p>
+            <p>Vous pouvez retrouver le code source de ce projet sur <a href="https://github.com/johan-jnn/factulandar" target="_blank">le repo GitHub</a>.</p>
+        </section>
+    </main>
+@endsection
