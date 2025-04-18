b95defde7b0051f1b215bf99c0f1209b126c3bfe
Johan J <johan.janin@gmail.com>
2025-04-18 13:47:14 +0200

    Better stats page

--- resources/views/pages/dashboard/client/index.blade.php
+++ resources/views/pages/dashboard/client/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('page')
     <h2>Statistiques de {{ $client->name }}</h2>
-    <ul>
+    <ul class="stats">
         <li>Factures: {{ $client->invoices->count() }}</li>
         @if ($calendar)
             <li>Evenements du calendrier: {{ count($calendar['events']) }}</li>
