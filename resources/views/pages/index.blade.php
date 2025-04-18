625db21a63739cd75f3212e161290857213ec764
Johan J <johan.janin@gmail.com>
2025-04-18 12:32:09 +0200

    Started better homepage and added app screenshots

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,6 +3,16 @@
 ])
 
 @section('body')
-    <x-hero title="L'application n°1 de la génération automatique de factures"
-        description="Créez votre compte et venez vous faciliter la vie !" />
+    <main>
+        <x-hero title="L'application n°1 de la génération automatique de factures"
+            description="Créez votre compte et venez vous faciliter la vie !" />
+
+        <section>
+            <h2>Présentation</h2>
+            <p>Grâce à Factulandar, générez vos factures pour tous vos clients :</p>
+
+            <h3>Créez votre société</h3>
+            <img src="/screenshots/societies.png" alt="Page des sociétés liées au compte">
+        </section>
+    </main>
 @endsection
