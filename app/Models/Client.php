2e95ec2390e56fc2eb91e05aead803e3194dce55
Johan J <johan.janin@gmail.com>
2026-02-09 21:59:05 +0100

    fix: url changeing on mathieutu api

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -7,40 +7,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    protected $fillable = [
-        "calendar_url",
-        "name",
-        "address",
-        "user_id",
-        "prefered_society",
-        "prefered_hours_price"
-    ];
+	protected $fillable = [
+		"calendar_url",
+		"name",
+		"address",
+		"user_id",
+		"prefered_society",
+		"prefered_hours_price"
+	];
 
-    public function invoices()
-    {
-        return $this->hasMany(Invoice::class, "client_id");
-    }
+	public function invoices()
+	{
+		return $this->hasMany(Invoice::class, "client_id");
+	}
 
-    public function calendar()
-    {
-        $json = Http::get('https://ical.mathieutu.dev/json', [
-            'url' => $this->calendar_url,
-        ]);
-        abort_if(!$json->ok(), $json->status());
-        return $json->json();
-    }
+	public function calendar()
+	{
+		$json = Http::get('https://ical.mathieutu.dev/json', [
+			'urls' => $this->calendar_url,
+		]);
+		abort_if(!$json->ok(), $json->status());
+		return $json->json();
+	}
 
-    public function nullable_calendar()
-    {
-        try {
-            return $this->calendar();
-        } catch (\Throwable $th) {
-            return null;
-        }
-    }
+	public function nullable_calendar()
+	{
+		try {
+			return $this->calendar();
+		} catch (\Throwable $th) {
+			return null;
+		}
+	}
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
 }
